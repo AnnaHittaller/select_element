03719ba5ef5e536b184a82a6d88f291d2ac26563
Anna Hittaller <70811887+AnnaHittaller@users.noreply.github.com>
2023-10-23 13:58:36 +0000

    multiselect ready

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,31 +1,51 @@
 import { useEffect, useState } from "react"
 import styles from "./select.module.css"
 
-type SelectOption = {
+export type SelectOption = {
     label: string,
     value: string | number
 }
 
-type SelectProps = {
-    options: SelectOption[]
-    value?: SelectOption
-    onChange: (value: SelectOption | undefined) => void
+type MultipleSelectProps = {
+  multiple: true
+  value: SelectOption[]
+  onChange: (value: SelectOption[]) => void
 }
 
-export function Select({value, onChange, options}: SelectProps) {
+type SingleSelectProps = {
+  multiple?: false
+  value?: SelectOption
+  onChange: (value: SelectOption | undefined) => void
+}
+
+type SelectProps = {
+  options: SelectOption[]
+} & (SingleSelectProps | MultipleSelectProps)
+
+export function Select({ multiple, value, onChange, options }: SelectProps) {
     const [isOpen, setIsOpen] = useState(false)
     const [hightlightedIndex, setHighlightedIndex] = useState(0)
 
     function clearOptions() {
-        onChange(undefined)
+        multiple ? onChange([]) : onChange(undefined)
     }
 
     function selectOption(option: SelectOption){
-        if(option !== value) onChange(option)
+        if(multiple) {
+            if(value.includes(option)) {
+                onChange(value.filter(o => o !== option))
+            } else {
+                onChange([...value, option])
+            }
+        } else {
+            
+            if(option !== value) onChange(option)
+        }
+
     }
 
     function isOptionSelected(option: SelectOption) {
-        return option === value
+        return multiple ? value.includes(option) : option === value
     }
 
     useEffect(() => {
@@ -35,7 +55,12 @@ export function Select({value, onChange, options}: SelectProps) {
     return (
         <div onBlur={() => setIsOpen(false)}
         onClick={() => setIsOpen(prev => !prev)} tabIndex={0} className={styles.container}>
-            <span className={styles.value}>{value?.label}</span>
+            <span className={styles.value}>{multiple ? (value.map(v => (
+                <button key={v.value} onClick={e => {
+                    e.stopPropagation()
+                    selectOption(v)
+                }} className={styles["option-badge"]}>{v.label}<span className={styles["remove-btn"]}>&times;</span></button>
+            ))) : value?.label}</span>
             <button onClick={e => {e.stopPropagation(); clearOptions()}} className={styles["clear-btn"]}>&times;</button>
             <div className={styles.divider}></div>
              <div className={styles.caret}></div>
@@ -54,3 +79,5 @@ export function Select({value, onChange, options}: SelectProps) {
       
     )
 }
+
+// make it WCAG compliant: ARIA roles + contrast 
